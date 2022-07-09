056fd470f0d801bc6f20dd7d79364d99a154fed4
kurotaku <kuroiwa.taku@gmail.com>
2022-07-09 13:56:28 +0900

    Revert "site controller作成"
    This reverts commit 628d0b3d81b49fedc2bfe1d5e2c3ac0cf974dd7f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root 'site#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
