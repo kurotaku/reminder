70694ad9410388c679aea2765fad9b1433106bce
kurotaku <kuroiwa.taku@gmail.com>
2022-07-10 11:54:11 +0900

    config/initializers/generators.rb編集

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -6,7 +6,7 @@ Rails.application.config.generators do |g|
   g.skip_routes true
   g.test_framework :rspec,
     fixtures: true,
-    request_specs: false,
+    request_specs: true,
     controller_specs: false,
     view_specs: false,
     helper_specs: false,
