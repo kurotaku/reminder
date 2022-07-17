16c7f5b270d624a283ae825dc2ebe60d5de59410
kurotaku <kuroiwa.taku@gmail.com>
2022-07-17 12:53:15 +0900

    store_pageのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'site#home'
 
+  namespace :store_page, path: ':store_ulid' do
+    resource :dashboard, only: %w[show], controller: 'dashboard'
+  end
+
   namespace :user_page, path: 'user' do
     resources :stores
   end
