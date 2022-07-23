d24a5fba87f105d1e290f170bf14778effd2f859
kurotaku <kuroiwa.taku@gmail.com>
2022-07-24 00:54:03 +0900

    Revert "顧客一覧アップデート"
    This reverts commit b2d139c6e464315108e7006ec9f72b77f783cced.

--- app/javascript/components/pages/customers/Index.js
+++ app/javascript/components/pages/customers/Index.js
@@ -4,8 +4,6 @@ import MainTitle from '../../uiParts/main/MainTitle'
 import MainContainer from '../../uiParts/main/MainContainer'
 import ContentTitle from '../../uiParts/title/Title'
 import { StoreUlid } from '../../Main'
-import TableHeader from '../../uiParts/table/TableHeader'
-import SeparateTableItem from '../../uiParts/table/SeparateTableItem'
 
 const Index = () => {
   const [customers, setCustomers] = useState([])
@@ -18,6 +16,7 @@ const Index = () => {
       }
     })
     .then(resp => {
+      console.log(resp.data)
       setCustomers(resp.data)
     })
     .catch(e => {
@@ -36,24 +35,9 @@ const Index = () => {
           buttonText="新規作成"
           buttonPath="/customers/new"
         />
-        <TableHeader>
-          {[
-            {'key': '1', 'label': '顧客名', 'width': '20'},
-            {'key': '2', 'label': '顧客番号', 'width': '20'},
-            {'key': '3', 'label': '電話番号', 'width': '20'},
-          ]}
-        </TableHeader>
         {
           customers.map((customer) => {
-            return (
-              <SeparateTableItem key={customer.id} path={customer.id}>
-                {[
-                  {'key': customer.full_name, 'label': customer.full_name, 'width': '20'},
-                  {'key': customer.number, 'label': customer.number, 'width': '20'},
-                  {'key': customer.display_phone, 'label': customer.display_phone, 'width': '20'},
-                ]}
-              </SeparateTableItem>
-            )
+            return <div key={customer.id}>{customer.phone}</div>
           }
         )}
       </MainContainer>
